dbd5d59638de0eb6a5d0a105a6fab987db44dfc4
tuhoang <htu25399@gmail.com>
2023-05-10 22:05:03 +0700

    add user-and-auth service

--- src/app/shared/services/user-and-authentication.service.ts
+++ src/app/shared/services/user-and-authentication.service.ts
@@ -1,9 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { User } from '../models';
+import { Observable } from 'rxjs';
 
 export interface LoginRequest {
   email: string;
   password: string;
+}
+
+export interface RegisterRequest extends LoginRequest {
+  username: string;
+}
+
+export interface UpdateProfileRequest extends RegisterRequest {
+  bio: string;
+  image: string;
 }
 
 @Injectable({
@@ -12,8 +23,24 @@ export interface LoginRequest {
 export class UserAndAuthenticationService {
   readonly #httpClient = inject(HttpClient);
 
-  login(user: LoginRequest) {
-    return this.#httpClient.post('/user/login', {
+  login(user: LoginRequest): Observable<User> {
+    return this.#httpClient.post<User>('/user/login', {
+      user,
+    });
+  }
+
+  register(user: RegisterRequest): Observable<User> {
+    return this.#httpClient.post<User>('/users', {
+      user,
+    });
+  }
+
+  getUserProfile(): Observable<User> {
+    return this.#httpClient.get<User>('/user');
+  }
+
+  updateUserProfile(user: UpdateProfileRequest): Observable<User> {
+    return this.#httpClient.put<User>('/user', {
       user,
     });
   }
