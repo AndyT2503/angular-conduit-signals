5b0cdbd0519c1957c1f8959e6d34aaa5082d5f05
tuhoang <htu25399@gmail.com>
2023-11-25 18:50:47 +0700

    use observable instead of signal for guard & edit name of guard

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,11 +1,15 @@
-import { inject } from "@angular/core";
-import { CanMatchFn, Router } from "@angular/router";
-import { AuthStore } from "../store";
+import { inject } from '@angular/core';
+import { CanMatchFn, Router } from '@angular/router';
+import { map, take } from 'rxjs';
+import { AuthStore } from '../store';
 
-export const nonAuthGuard: CanMatchFn = () => {
-    const authStore = inject(AuthStore);
-    const router = inject(Router);
-    return (
-      !authStore.selectors.isAuthenticated() ?? router.createUrlTree(['/'])
+export const authGuard: CanMatchFn = () => {
+  const authStore = inject(AuthStore);
+  const router = inject(Router);
+  return authStore
+    .select((x) => x.isAuthenticated)
+    .pipe(
+      map((isAuth) => isAuth ?? router.createUrlTree(['/login'])),
+      take(1)
     );
-  };
+};
--- src/app/shared/guards/non-auth.guard.ts
+++ src/app/shared/guards/non-auth.guard.ts
@@ -1,11 +1,15 @@
 import { inject } from '@angular/core';
 import { CanMatchFn, Router } from '@angular/router';
+import { map, take } from 'rxjs';
 import { AuthStore } from '../store';
 
-export const authGuard: CanMatchFn = () => {
+export const nonAuthGuard: CanMatchFn = () => {
   const authStore = inject(AuthStore);
   const router = inject(Router);
-  return (
-    authStore.selectors.isAuthenticated() ?? router.createUrlTree(['/login'])
-  );
+  return authStore
+    .select((x) => x.isAuthenticated)
+    .pipe(
+      map((isAuth) => !isAuth ?? router.createUrlTree(['/'])),
+      take(1)
+    );
 };
